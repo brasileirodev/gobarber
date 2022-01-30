5f3feff8ffc371f94913e3f0b297d19e0abbac69
Marcel Brasileiro <brasileirodev@gmail.com>
2022-01-30 10:03:16 -0300

    fix bug of trying to create an appointment without a valid-user

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,6 +1,7 @@
 import Appointment from '@models/appointment';
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 import AppointmentsRepository from '@repositories/AppointmentsRepository';
+import User from '@models/User';
 import { startOfHour } from 'date-fns';
 
 interface RequestDTO {
@@ -11,8 +12,16 @@ interface RequestDTO {
 class CreateAppointmentService {
   public async execute({ provider_id, date }: RequestDTO): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const usersRepository = getRepository(User);
     const appointmentDate = startOfHour(date);
     const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate);
+    const userExists = await usersRepository.findOne({
+      where: { id: provider_id },
+    });
+
+    if (!userExists) {
+      throw new Error('Don\'t exist a user with this provider_id.');
+    }
 
     if (findAppointmentInSameDate) {
       throw Error('This appointment is already booked');
