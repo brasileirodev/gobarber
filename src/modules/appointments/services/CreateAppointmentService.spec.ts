e55b38d94e9ec904a8ed6d5355d72a1e4a1c331c
Marcel Brasileiro <brasileirodev@gmail.com>
2022-02-03 20:36:44 -0300

    Testando o throw AppError

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,4 +1,5 @@
 import CreateUserService from '@modules/users/services/CreateUserService';
+import AppError from '@shared/errors/AppError';
 import CreateAppointmentService from './CreateAppointmentService';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import FakeUsersRepository from '../../users/repositories/fakes/FakeUsersRepository';
@@ -27,7 +28,49 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe(user.id);
   });
 
-  // it('should not be able to create two appointmets on the same time', async () => {
-  //   expect(true).toBe(true);
-  // });
+  it('should not be able to create two appointmets on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+      fakeUsersRepository,
+    );
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'john@gmail.com',
+      password: '123456',
+    });
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    const appointmentOnce = await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: user.id,
+    });
+
+    expect(appointmentOnce).toHaveProperty('id');
+    expect(appointmentOnce.provider_id).toBe(user.id);
+    expect(createAppointment.execute({
+      date: appointmentDate,
+      provider_id: user.id,
+    })).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create appointmets with an user that does not exist', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+      fakeUsersRepository,
+    );
+
+    const appointmentDate = new Date();
+
+    expect(createAppointment.execute({
+      date: appointmentDate,
+      provider_id: 'non-existing-user-id',
+    })).rejects.toBeInstanceOf(AppError);
+  });
 });
